68db3a890dc4128f884b27a9eb8fdb2e551a6e74
hindatoun <mahach.hinda@gmail.com>
2023-03-20 16:54:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + 0);
+	}
+	putchar('\n');
+	return (0);
+}
