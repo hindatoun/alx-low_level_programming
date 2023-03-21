6d46fc69091257d7e5fc27eeee33d8860cf7cc2f
hindatoun <mahach.hinda@gmail.com>
2023-03-20 17:20:57 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n <= 9)
+		{
+			putchar(',');
+			putchar(" ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
