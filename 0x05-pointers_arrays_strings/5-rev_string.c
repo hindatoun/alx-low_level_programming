0a9c537eb6fcc3acf6c2a44f27b09ba338dde4fc
hindatoun <mahach.hinda@gmail.com>
2023-03-28 16:00:08 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string input
+ *
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+
+	int hind = 0;
+
+	int i;
+
+	while (s[hind] != '\0')
+	{
+		hind++;
+	}
+	for (i = 0; i < hind; i++)
+	{
+		hind--;
+		rev = s[i];
+		s[i] = s[hind];
+		s[hind] = rev;
+	}
+}
