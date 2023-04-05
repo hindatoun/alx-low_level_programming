fa09c4283bca1a06871e5836c2eab7b0cd258748
hindatoun <mahach.hinda@gmail.com>
2023-04-04 17:38:23 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strstr - locates a substring
+ *
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return:  located substring, or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		while (*haystack == *needle && *needle != '\0')
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
