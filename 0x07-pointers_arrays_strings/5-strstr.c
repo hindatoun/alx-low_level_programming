a5c093e153ca160b92bc50c0a88979dfc00f4729
hindatoun <mahach.hinda@gmail.com>
2023-04-04 17:44:11 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		while (*haystack == *needle && *needle != '\0')
+		while (haystack == needle && needle != '\0')
 		{
 			haystack++;
 			needle++;
 		}
-		if (*needle == '\0')
+		if (needle == '\0')
 		{
 			return (haystack);
 		}
