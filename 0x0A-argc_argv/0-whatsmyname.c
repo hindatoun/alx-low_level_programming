1c6d252bc03dcea23a40869613d6a355a5cdb5ed
hindatoun <mahach.hinda@gmail.com>
2023-04-10 17:06:53 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line
+ *
+ * @argc: count of argument
+ * @argv: array of pointers to the string
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	print("%s\n", argv[argc - 1]);
+	return (0);
+}
